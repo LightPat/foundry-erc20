897335eea1f62c9e3d3ded8afccd47055937b969
Patrick Seeman <patseem189@gmail.com>
2026-02-08 11:03:53 -0500

    update import statements

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
 import {OurToken} from "src/OurToken.sol";
-import {DeployOurToken} from "script/DeployOurToken.sol";
+import {DeployOurToken} from "script/DeployOurToken.s.sol";
 
 contract OurTokenTest is Test {
     OurToken public ourToken;
